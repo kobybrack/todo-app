f7894c9bea564d97612b525af7bf90a407fd9f72
Koby Brackebusch <koby@goodcode.net>
2023-02-15 22:28:24 -0800

    got electron set up with debugging!

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('I love Kiden!');
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+
+    win.loadFile('views/index.html');
+};
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
